6dfa4c72393f72116e1899ca0fd203c4edeec234
Legolas1086 <sanjayurs16@gmail.com>
2021-06-20 11:40:01 +0530

    side bar added

---
+++ frontend/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Side from './sede.js'
+
+function Home(){
+    return(
+        <div className="Home_page">
+            <Side/>
+
+        </div>
+    )
+}
+
+export default Home;
