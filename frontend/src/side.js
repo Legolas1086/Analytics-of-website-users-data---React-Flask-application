d67a631bdba1e0baa39d364254a88f4382f1255c
Legolas1086 <sanjayurs16@gmail.com>
2021-06-25 21:51:10 +0530

    page refresh bug solved

--- frontend/src/side.js
+++ frontend/src/side.js
@@ -14,24 +14,30 @@ class Side extends React.Component{
     handleClick(event){
         this.setState({active:event.target.name})
         console.log(this.state.active)
+        const path = window.location.pathname
+        console.log(path)
     }
 
+    
+
     render(){
+        const path = window.location.pathname
+       
         const active_style={
             backgroundColor:"rgb(209, 234, 237)",
             padding:"1rem",
             borderTopRightRadius:"50px",
             borderBottomRightRadius:"50px",
             color:"blue"
-
         }
 
         return(
+            
             <div className="Side_bar">
-                <Link to="/" name="home" style={this.state.active=="home"? active_style:null} onClick={this.handleClick}>Home</Link>
-                <Link to="/future" name="future" style={this.state.active=="future"? active_style:null} onClick={this.handleClick}>Future</Link>
-                <Link to="/visualise" name="visualise" style={this.state.active=="visualise"? active_style:null} onClick={this.handleClick}>Visualise</Link>
-                <Link to="/countries" name="countries" style={this.state.active=="countries"? active_style:null} onClick={this.handleClick}>Countries</Link>
+                <Link to="/" name="home" style={path=="/"? active_style:null} onClick={this.handleClick}>Home</Link>
+                <Link to="/future" name="future" style={path=="/future"? active_style:null} onClick={this.handleClick}>Future</Link>
+                <Link to="/visualise" name="visualise" style={path=="/visualise"? active_style:null} onClick={this.handleClick}>Visualise</Link>
+                <Link to="/countries" name="countries" style={path=="/countries"? active_style:null} onClick={this.handleClick}>Countries</Link>
             </div>
         )
     }
